f5219897972541a9ce2dd1fa2a7162de13191f01
Raju komati <komatiraju032@gmail.com>
2021-09-04 20:46:20 +0530

    added custom Exception class

---
+++ src/main/java/com/rkomati/jrc/exception/CompilationFailureException.java
@@ -0,0 +1,8 @@
+package com.rkomati.jrc.exception;
+
+public class CompilationFailureException extends Exception {
+
+    public CompilationFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
